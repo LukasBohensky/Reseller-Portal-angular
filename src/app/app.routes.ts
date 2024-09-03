50a99a0ea3d3fe685c0c028c360af0f440708f53
Elias <elias.epet@htl-wels.at>
2024-09-03 14:24:48 +0200

    commit1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },  // Route für die Home-Komponente
+  { path: '**', redirectTo: '' }  // Alle nicht gefundenen Routen zur Home-Seite umleiten
+];
