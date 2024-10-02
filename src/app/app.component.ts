bb249d8e1b3851b592eff150254bd8e98801c640
Lukas Bohensky <lukas.bohensky@htl-wels.at>
2024-10-02 19:57:26 +0200

    commit design prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
+import {MatCommonModule} from '@angular/material/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from "./sidenav/sidenav.component";
 import { RouterModule } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from "@angular/material/icon";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidenavComponent, RouterModule],
+  imports: [RouterOutlet, SidenavComponent, RouterModule, MatTooltipModule, MatCommonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { StoreComponent } from './store/store.component';
 
 export const routes: Routes = [
   {path:'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'store', component: StoreComponent}
 ];
